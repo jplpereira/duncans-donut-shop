6e1148beb5268042e1bff4982453b796829d21dc
João Pedro Pereira <jplpereira@gmail.com>
2016-09-19 03:00:45 -0300

    Initial commit

---
+++ src/duncan.js
@@ -0,0 +1,47 @@
+function greatings(){
+  alert("Welcome to Duncan's Just-In-Time Donuts!");
+}
+
+function id(element){
+  return document.getElementById(element);
+}
+
+function getNumCakeDonuts(){
+  var numCakeDonuts = 0;
+  numCakeDonuts = parseFloat(id("cakedonuts").value);
+  return numCakeDonuts;
+}
+
+function getNumGlazedDonuts(){
+  var numGlazedDonuts = 0;
+  numGlazedDonuts = parseFloat(id("glazeddonuts").value);
+  return numGlazedDonuts;
+}
+
+function getSubTotal(){
+  const DONUTPRICE = 0.50;
+  var subTotal = 0;
+  subTotal = ( getNumCakeDonuts() + getNumGlazedDonuts() ) * DONUTPRICE;
+  return subTotal;
+}
+
+function getTax(){
+  const TAXRATE = 0.07;
+  var tax = 0;
+  tax = getSubTotal() * TAXRATE;
+  return tax;
+}
+
+function getTotal(){
+  var total = 0;
+  total = getSubTotal() + getTax();
+  return total;
+}
+
+function updateOrder(){
+  const NICEDAY = "Have a nice day!"
+  id("subtotal").value = "$" + getSubTotal().toFixed(2);
+  id("tax").value = "$" + getTax().toFixed(2);
+  id("total").value = "$" + getTotal().toFixed(2);
+  id("niceday").innerHTML = NICEDAY;
+}
